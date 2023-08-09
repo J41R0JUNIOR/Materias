c7c1e92d36c85a60b776413d8a2a9bbc1019050d
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-08-09 08:08:16 -0300

    add

---
+++ Programação Orientada a Objetos/Testes/Testes/Teste01.java
@@ -0,0 +1,7 @@
+package Testes;
+
+public class Teste01 {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Se liga nesse HELLO WORLD my friend!");
+    }
+}
