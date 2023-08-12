00a5cc96ce41fa6a2b9bbb536185b5b0e14f8cf6
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-08-12 12:05:57 -0300

    add

---
+++ Estrutura de Dados/Atividades/atividade1/verifica_numero_primo.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    
+    int verificar(int n);
+    int n = 0;
+
+    n = atoi(argv[1]); 
+
+
+    if(verificar(n) == 1){
+        printf("Numero Primo");
+    }else{
+        printf("Numero Nao Primo");
+    }
+
+    return 0;
+}
+
+
+int verificar(int n){
+    int qtd = 0;
+
+    if((n % 2) != 0){
+        for(int i = n ; i != 0; i--){
+            if((n % i) == 0){
+                qtd++;
+            }
+        }  
+
+        if(qtd <= 2){
+            return 1;
+        }else{
+            return 0;
+        }
+    }else{
+        return 0;
+    }
+}
