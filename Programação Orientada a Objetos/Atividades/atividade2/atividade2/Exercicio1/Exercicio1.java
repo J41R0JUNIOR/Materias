6c2f8d603e640f2a4430d5f32a567b9ae07aa3fb
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-08-20 17:31:44 -0300

    j

--- Programação Orientada a Objetos/Atividades/atividade2/atividade2/Exercicio1/Exercicio1.java
+++ Programação Orientada a Objetos/Atividades/atividade2/atividade2/Exercicio1/Exercicio1.java
@@ -1,12 +1,14 @@
 package atividade2.Exercicio1;
-import atividade2.Exercicio1.Calculadora.Calculadora;
-
 
 public class Exercicio1 {
     
+    public int a,b;
+    private String operador;
+    public char[] resposta;
+
     public static void main(String[] args) throws Exception{
 
-        Calculadora calc = new Calculadora();
+        Exercicio1 calc = new Exercicio1();
 
 
         calc.a = 3;
@@ -16,7 +18,66 @@ public class Exercicio1 {
 
         System.out.println(calc.operar());
     }
+    
 
+    public String getOperador(){
+        return this.operador;
+    }
+
+    public void setOperador(String op) throws Exception{
+        Boolean erro = true;
+
+        switch (op) {
+            case "+":
+                erro = false;
+                break;
+            case "-":
+                erro = false;
+                break;
+            case "*":
+                erro = false;
+                break;
+            case "/":
+                erro = false;
+                break;
+            default:
+            erro = true;
+                break;
+        }
+
+        if(erro){
+            throw new Exception("Operador Inválido!");
+        }
+        this.operador = op;
+    }
+
+    public int operar(){
+
+        int ret = 0;
+
+        switch (this.operador) {
+            case "+":
+                ret = this.a + this.b;
+                break;
+            case "-":
+                ret = this.a - this.b;
+                break;
+            case "*":
+                ret = this.a * this.b;
+                break;
+            case "/":
+                ret = this.a / this.b;
+                break;
+            default:
+                break;
+        }
+        return ret;
+    }
+
+
+    public void setOperadors(String string, String string2) {
+    }
+    
     
 }
 
--- Programação Orientada a Objetos/Atividades/atividade2/atividade2/Exercicio2/Exercicio2.java
+++ Programação Orientada a Objetos/Atividades/atividade2/atividade2/Exercicio2/Exercicio2.java
@@ -1,18 +1,79 @@
 package atividade2.Exercicio2;
-import atividade2.Exercicio2.Calculadora2.Calculadora2;
+
 
 public class Exercicio2 {
-    
+    public float a, b;
+    private String operador1, operador2;
+    public float resposta;
+
+
     public static void main(String[] args) throws Exception{
 
-        Calculadora2 calc = new Calculadora2();
+        Exercicio2 calc = new Exercicio2();
 
 
-        calc.a = 32;
+        calc.a = 0;
         
-        calc.setOperadors("f", "f");
+        calc.setOperadors("c", "f");
 
         System.out.print(calc.resposta);
+        
+    }
+
+
+    public void setOperadors(String op, String op2) throws Exception{
+        Boolean erro1 = true;
+        boolean erro2 = true;
+
+        switch (op) {
+            case "f":
+                erro1 = false;
+                break;
+            case "c":
+                erro1 = false;
+                break;
+            default:
+            erro1 = true;
+                break;
+        }
+        switch (op2) {
+            case "f":
+                erro2 = false;
+                break;
+            case "c":
+                erro2 = false;
+                break;
+            default:
+            erro2 = true;
+                break;
+        }
+
+        if(erro1){
+            throw new Exception("Operador 1 Inválido!");
+        }
+        if(erro2){
+            throw new Exception("Operador 2 Inválido!");
+        }
+
+        
+        this.operador1 = op;
+        this.operador2 = op2;
+        converter(this.a, this.b);
+    }
+
+    public void converter(Float a, Float b)  throws Exception{
+        
+        if(operador1 == "c" && operador2 == "f"){
+            //this.resposta = (a/5) / ((b-32)/9);
+            this.resposta = (a * 9/5) + 32;
+        }
+        else if(operador1 == "f" && operador2 == "c"){
+            this.resposta = (a - 32) * 5/9;
+            //this.resposta = ((a-32)/9) / (b/5);
+        }
+        else{
+            throw new Exception("Operadores iguais");
+        }
         
     }
 }
