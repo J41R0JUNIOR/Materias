7b46d031e6fb10094deb28767dfd47ed1fde97a7
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-08-25 10:16:01 -0300

    add

--- Programação Orientada a Objetos/Atividades/atividade2/atividade2/Exercicio2/Exercicio2.java
+++ Programação Orientada a Objetos/Atividades/atividade2/atividade2/Exercicio2/Exercicio2.java
@@ -1,5 +1,6 @@
 package atividade2.Exercicio2;
 
+import java.util.Scanner;
 
 public class Exercicio2 {
     public float a, b;
@@ -13,15 +14,40 @@ public class Exercicio2 {
 
 
         calc.a = 0;
+        // Cria um objeto Scanner para ler entradas do terminal
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor: ");
+        float valor = scanner.nextFloat(); 
+
+        scanner.nextLine();
+
+        System.out.printf("f para Fahreneit e c para Celsius \n valor digitado é:");
+        String op = scanner.nextLine(); // Lê a linha digitada pelo usuário
+
+        System.out.print("e voce quer transformar em:");
+        String op2 = scanner.nextLine(); 
+
         
-        calc.setOperadors("c", "f");
+
+        // Não se esqueça de fechar o Scanner quando terminar de usá-lo
+        scanner.close();
+
+
+        System.out.println(op);
+        System.out.println(op2);
+
+
+
+       calc.a = valor;
+        calc.setOperadors(op, op2, "foi");
 
         System.out.print(calc.resposta);
         
     }
 
 
-    public void setOperadors(String op, String op2) throws Exception{
+    public void setOperadors(String op, String op2, String string) throws Exception{
         Boolean erro1 = true;
         boolean erro2 = true;
 
@@ -61,19 +87,20 @@ public class Exercicio2 {
         converter(this.a, this.b);
     }
 
-    public void converter(Float a, Float b)  throws Exception{
-        
-        if(operador1 == "c" && operador2 == "f"){
-            //this.resposta = (a/5) / ((b-32)/9);
+    public void converter(Float a, Float b) throws Exception {
+
+        if (this.operador1.equals("c") && this.operador2.equals("f")) {
             this.resposta = (a * 9/5) + 32;
-        }
-        else if(operador1 == "f" && operador2 == "c"){
+
+        } else if (this.operador1.equals("f") && this.operador2.equals("c")) {
             this.resposta = (a - 32) * 5/9;
-            //this.resposta = ((a-32)/9) / (b/5);
-        }
-        else{
+
+        } else if (this.operador1.equals("f")) {
+            System.out.print(operador1);
+
+        } else {
             throw new Exception("Operadores iguais");
         }
-        
     }
+    
 }
