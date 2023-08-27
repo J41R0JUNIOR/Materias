374a88cfd9e55d1b0621d1a4c72d3249eedf6fc0
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-08-27 16:49:51 -0300

    add

--- Estrutura de Dados/Atividades/atividade3/regressao_linear.c
+++ Estrutura de Dados/Atividades/atividade3/regressao_linear.c
@@ -18,11 +18,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Read data until the end of the file
     while (fscanf(file, "%i,%f", &x, &y) != EOF) {
-    //    printf("%i , %f\n", x, y);
-        qtd++;
+       qtd++;
     }
+    x = 0;
+    y = 0;
 
     // Reset the file pointer to the beginning of the file
     fseek(file, 0, SEEK_SET);
@@ -35,11 +35,37 @@ int main(int argc, char* argv[]) {
     
     for (int i = 0; i < qtd; i++) {
         fscanf(file, "%i,%f", &pontos[i].x, &pontos[i].y);
-        printf("\n%i, %f", pontos[i].x, pontos[i].y);
+        fflush(stdin);
+    //    printf("\n%i, %f", pontos[i].x, pontos[i].y);
     }
 
     printf("\nquantidade de registros = %i\n", qtd);
     fclose(file);
+
+    //media x e y
+    for(int i = 0; i < qtd; i++){
+        x += pontos[i].x;
+        y += pontos[i].y;
+    }
+ 
+    float mediaX = (float)x / qtd;
+    float mediaY = y / qtd;
+    printf("\ntotal x = %d, total y = %f", x, y);
+    printf("\nmedia x = %i, media y = %f", mediaX, mediaY);
+
+    //(coeficiente angular)
+    //inclinacao 
+    float inclinacao;
+    for(int i = 0; i < qtd; i++){
+        inclinacao = (pontos[i].x - mediaX) * (pontos[i].y - mediaY) / ((pontos[i].x - mediaX) * (pontos[i].x - mediaX));
+    }
+
+    printf("\n inclinacao = %f", inclinacao);
+
+    //(coeficiente linear)
+    //intercepcao
+    float intercepcao = mediaY - inclinacao * mediaX;
+    printf("\n intercepcao = %f", intercepcao);
 
     return 0;
 }
