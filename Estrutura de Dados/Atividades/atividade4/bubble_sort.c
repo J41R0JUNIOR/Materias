990780ebf99d8bfd9cf89079318787bbc8654320
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-09-16 21:56:54 -0300

    s

--- Estrutura de Dados/Atividades/atividade4/bubble_sort.c
+++ Estrutura de Dados/Atividades/atividade4/bubble_sort.c
@@ -7,7 +7,7 @@ void swap(int* n1, int* n2){
     *n1 = *n2;
     *n2 = aux;
 }
-
+ 
 void bubble_sort(int *arr, int n){
     for(int i = 0; i < n - 1; i ++){
             for(int j = 0; j<(n-1-i); j++){
--- Estrutura de Dados/Atividades/atividade4/main.c
+++ Estrutura de Dados/Atividades/atividade4/main.c
@@ -2,26 +2,78 @@
 #include <stdlib.h>
 #include "bubble_sort.h"
 
-void imprimir_arr(int *arr, int n){
-    for(int i = 0; i < n;i++){
-        printf("%d - ", arr[i]); 
+// gcc main.c bubble_sort.c -o main
+// ./main mainarquivoDeLeitura.txt
+
+void imprimir_arr(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+       
+        if(i == n-1){
+            printf("%d", arr[i]);
+        }else{
+            printf("%d,", arr[i]);
+        }
     }
     printf("\n");
 }
 
 
-int main(int argc, char* argv[]){
 
-    int arr[] = {54,26,93,17,77,31,44,55,20};
-    int n = sizeof(arr) / sizeof(int);
+void fileAtt(int *arr, int n){
+    FILE* file;
+    file = fopen("ArrayOrdenado.txt", "wt");
+    
 
-    imprimir_arr(arr,n);
-    bubble_sort(arr,n);
-    imprimir_arr(arr,n);
+    if (file == NULL) {
+        perror("Error opening file");
+        return ;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if(i == n-1){
+            fprintf(file, "%d", arr[i]);
+        }else{
+            fprintf(file, "%d, ", arr[i]); 
+        }
+    }
+
+    fclose(file);
+
+     printf("Dentro do arquivo Ordenado: ");
+    imprimir_arr(arr, n);
+
+}
+
+
+
+int main(int argc, char* argv[]) {
+    FILE* file;
+    file = fopen(argv[1], "r");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int arr[100];
+    int j = 0;
+
+    while (fscanf(file, "%i,", &arr[j]) != EOF) {
+        j++;
+    }
+
+    int  n = j; 
+
+    fclose(file);
+
+    printf("Dentro do arquivo de Leitura: ");
+    imprimir_arr(arr, n);
+    bubble_sort(arr, n);
+
+    fileAtt(arr, n);
+
+   
 
     return 0;
 }
-/*
-gcc main.c bubble_sort.c -o main
-./main 
-*/
+
