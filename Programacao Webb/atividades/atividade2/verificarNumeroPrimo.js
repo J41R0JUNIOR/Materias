b05c8e268f3aab1db99796a9ea8157f781e1272a
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-03-20 10:36:15 -0300

    add

--- Programacao Webb/atividades/atividade2/verificarNumeroPrimo.js
+++ Programacao Webb/atividades/atividade2/verificarNumeroPrimo.js
@@ -1,15 +1,15 @@
 function verificarNumeroPrimo(n){
-    
     let qtd = 0
 
-    for (let i = 0; i <= n; i++) {
-            if(i > 2){
-                console.log('opa')
-                break;
-            }else if(n % i == 0){
-                qtd += 1
-            }  
-       }
+    for (let i = 0; i <=  n; i++) {
+        if(i > 2){
+            return false
+            // break;
+        }else if(n % i == 0){
+            qtd += 1
+        }  
+    }
+
     if(qtd <= 2 && n != 1 && n != 0){
        return true
     }else{
@@ -26,3 +26,4 @@ console.log(verificarNumeroPrimo(100)); // false
 console.log(verificarNumeroPrimo(991)); // true
 console.log(verificarNumeroPrimo(104729)); // true
 console.log(verificarNumeroPrimo(14348907)); // false
+console.log(verificarNumeroPrimo(1583209584803485093485094850983095830958408350948905385938905983095894305893058309485903859034859038058340583490354976)); 
