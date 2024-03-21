f16295d700677fb73dae5bba5a1607a9e419ecc2
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-03-21 20:58:56 -0300

    add

---
+++ Programacao Webb/atividades/atividade3/matrizTransposta.js
@@ -0,0 +1,33 @@
+function transporMatriz(A) {
+    console.log("Matriz original:");
+    imprimirMatriz(A);
+
+    const linhas = A.length;
+    const colunas = A[0].length;
+
+    const matrizTransposta = [];
+    for (let j = 0; j < colunas; j++) {
+        matrizTransposta[j] = [];
+    }
+
+    for (let i = 0; i < linhas; i++) {
+        for (let j = 0; j < colunas; j++) {
+            matrizTransposta[j][i] = A[i][j];
+        }
+    }
+
+    console.log("Matriz transposta:");
+    imprimirMatriz(matrizTransposta);
+}
+
+function imprimirMatriz(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+        console.log(matriz[i].join("\t"));
+    }
+}
+
+const matriz = [
+    [1,2],[3,4],[5,6]
+];
+
+transporMatriz(matriz);
