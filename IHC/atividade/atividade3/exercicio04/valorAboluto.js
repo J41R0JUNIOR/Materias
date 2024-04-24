ab709671d1a26e2a2fd27369ed597f62866b50bb
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-04-24 12:49:01 -0300

    adicionando todas as atividades

--- IHC/atividade/atividade3/exercicio04/valorAboluto.js
+++ IHC/atividade/atividade3/exercicio04/valorAboluto.js
@@ -1,8 +1,11 @@
 function calcularValorAbsoluto() {
- 
-    var inputNumber = document.getElementById("number").value;
+    var inputNumber = document.getElementById("number").value.trim(); // Remove espaços em branco do início e do fim
+    var absoluteValue = Math.abs(parseFloat(inputNumber)); // Calcula o valor absoluto
 
-    var absoluteValue = Math.abs(parseFloat(inputNumber));
-
-    document.getElementById("result").innerText = "O valor absoluto é: " + absoluteValue;
+    // Verifica se o valor inserido é válido antes de exibir o resultado
+    if (!isNaN(absoluteValue)) {
+        document.getElementById("result").innerText = "O valor absoluto é: " + absoluteValue;
+    } else {
+        document.getElementById("result").innerText = "Por favor, insira um número válido.";
+    }
 }
