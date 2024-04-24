ebf8ead9944c884b4b284a43fffd32feae2c2abd
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-04-24 10:43:37 -0300

    exercicio de pw

---
+++ Programacao Webb/testes/app_html/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express')
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views')
+
+app.get('/', function(req, res){
+    let nome = "Jairo"
+    res.render('index.html', {nome});
+});
+
+app.get('/contato', function(req, res){
+    res.render('contato.html');
+});
+
+const PORT = 8080;
+app.listen(PORT, function(){
+    console.log('app rodando na porta ', PORT);
+})
