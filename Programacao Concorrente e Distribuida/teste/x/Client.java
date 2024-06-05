9b0dbd31f33b0b91a458b1fe3b13b93cc882b482
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-06-05 13:05:00 -0300

    a

---
+++ Programacao Concorrente e Distribuida/teste/x/Client.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedInputStream bufferedInputStream;
+    private PrintWriter mandarProServidor;
+    String mensagem;
+
+    public Client(Socket socket) {
+        try {
+            this.socket = socket;
+            this.mensagem = "Opa meu chapa";
+            this.bufferedInputStream = new BufferedInputStream(socket.getInputStream());
+            this.bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream));
+            this.mandarProServidor = new PrintWriter(socket.getOutputStream(), true);
+
+            mandarProServidor.println(mensagem);
+
+            System.out.println("Server Answer: " + bufferedReader.readLine());
+        
+            closeEverything(this.socket, this.bufferedReader, this.bufferedInputStream);
+
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedInputStream);
+        }
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedInputStream bufferedInputStream) {
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedInputStream != null) {
+                bufferedInputStream.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("10.130.129.103", 12345);
+        Client client = new Client(socket);
+        client.listenForMessage();
+    }
+}
--- Programacao Webb/atividades/atividade7/app_calc/index.js
+++ Programacao Webb/atividades/atividade7/app_calc/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const path = require('path');
+// const path = require('path');
 const app = express();
 
 // Configurações
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'src', 'views'));
+// app.set('views', path.join(__dirname, 'src', 'views'));
+app.set('views',__dirname, 'src/views');
 app.use(express.urlencoded({ extended: true }));
 
 // Rotas
